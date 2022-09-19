63dd3a594ea09e2c3b6dcf5086dc33fa17f0309f
Murad <abishlimurad@gmail.com>
2022-09-19 02:29:37 +0200

    slider

--- source/js/common.js
+++ source/js/common.js
@@ -27,4 +27,10 @@ if (menuMobLinks.length > 0) {
 }
 
 
-
+let stepsSlider = new Swiper(".steps__slider", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  speed: 600
+  // fadeEffect: { crossFade: true },
+  // effect: 'fade'
+});
