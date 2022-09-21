bfe113f4a9514049b89773730bf9d3f47eb511e8
Murad <abishlimurad@gmail.com>
2022-09-21 14:19:44 +0200

    aos animations

--- source/js/_slider.js
+++
@@ -1,8 +0,0 @@
-let stepsSlider = new Swiper(".steps__slider", {
-  slidesPerView: 1,
-  spaceBetween: 24,
-  speed: 600,
-  autoplay: {
-    delay: 2000
-  }
-});
--- source/js/common.js
+++ source/js/common.js
@@ -14,6 +14,16 @@ burger.addEventListener('click', () => {
   }
 })
 
+let stepsSlider = new Swiper(".steps__slider", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  speed: 600,
+  autoplay: {
+    delay: 2000
+  }
+});
+
+
 const menuMobLinks = document.querySelectorAll('.menu-nav__link');
 
 if (menuMobLinks.length > 0) {
