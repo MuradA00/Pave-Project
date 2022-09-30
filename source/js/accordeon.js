d28d6e4912717b1a5f4cbfdb5d24260287b2bf76
Murad <abishlimurad@gmail.com>
2022-09-30 21:37:17 +0300

    fixes 30.09

--- source/js/accordeon.js
+++ source/js/accordeon.js
@@ -1,10 +1,20 @@
-const acc = document.getElementsByClassName("faq-row__item");
-const icons = document.getElementsByClassName('faq-row__icon')
+var acc = document.getElementsByClassName("faq-row__visible");
+const hiddenContent = document.querySelector('.faq-row__hidden')
 var i;
+const mobIcon = document.querySelectorAll('.mob-icon')
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("collapse");
+    var panel = this.nextElementSibling;
+    if (panel.style.height) {
+      panel.style.height = null;
+      panel.style.margin = '0px';
+      panel.style.overflow = 'hidden'
+    } else {
+      panel.style.margin = '0px 24px 30px';
+      panel.style.overflow = 'unset'
+      panel.style.height = panel.scrollHeight + "px";
+    }
   });
-};
-
+}
