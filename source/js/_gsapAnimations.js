fdccb76b9b91db6c3a56cf1f8590f5d42b206341
Murad <abishlimurad@gmail.com>
2023-02-09 04:06:14 +0200

    test

---
+++ source/js/_gsapAnimations.js
@@ -0,0 +1,181 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const firstBlock = document.querySelector('._first-block')
+const secondBlock = document.querySelector('._second-block')
+const thirdBlock = document.getElementById('heading3'),
+      imgSecond = document.querySelector('#img2'),
+      imgThird = document.querySelector('#img3');
+
+function setScrollText(){
+  gsap.to('.steps__image', {
+    scrollTrigger: {
+      trigger: '.steps__name'
+    },
+    opacity: 1,
+    start: '0s'
+  })
+
+  gsap.to('._first-block', {
+    scrollTrigger: {
+      trigger: '._first-block',
+      toggleActions: 'play reverse play reverse',
+      start: '-500s',
+      end: '+=1250s',
+      onEnter() {
+        firstBlock.classList.add('_active-block');
+      },
+      onLeave() {
+        firstBlock.classList.remove('_active-block');
+      },
+      onEnterBack() {
+        firstBlock.classList.add('_active-block');
+      },
+      onLeaveBack() {
+        firstBlock.classList.remove('_active-block');
+      }
+    },
+    opacity: 1,
+  });
+
+  gsap.to('._second-block', {
+    scrollTrigger: {
+      trigger: '._second-block',
+      toggleActions: 'play reverse play reverse',
+      start: '+=750s',
+      end: '+=1500s',
+      onEnter() {
+        secondBlock.classList.add('_active-block');
+      },
+      onLeave() {
+        secondBlock.classList.remove('_active-block');
+      },
+      onEnterBack() {
+        secondBlock.classList.add('_active-block');
+      },
+      onLeaveBack() {
+        secondBlock.classList.remove('_active-block');
+      }
+    },
+    opacity: 1
+  });
+
+  gsap.to('#heading3', {
+    scrollTrigger: {
+      trigger: '#heading3',
+      toggleActions: 'play reverse play reverse',
+      start: '+=2235s',
+      end: '+=3000s',
+      onEnter() {
+        thirdBlock.classList.add('_active-block');
+      },
+      onLeave() {
+        thirdBlock.classList.remove('_active-block');
+      },
+      onEnterBack() {
+        thirdBlock.classList.add('_active-block');
+      },
+      onLeaveBack() {
+        thirdBlock.classList.remove('_active-block');
+      }
+    },
+    opacity: 1,
+    toggleClass: "_active-block"
+  });
+
+}
+
+function setScrollImages() {
+
+  gsap.to('#img1', {
+    scrollTrigger: {
+      trigger: '#img1',
+      toggleActions: 'play reverse play reverse',
+      start: '0s',
+      end: '+=750s',
+    },
+
+  });
+
+  gsap.to('#img2', {
+    scrollTrigger: {
+      trigger: '#img2',
+      toggleActions: 'play reverse play reverse',
+      start: '+=850s',
+      end: '+=2235s',
+      onEnter() {
+        imgSecond.classList.add('_active-img');
+      },
+      // onLeave() {
+      //   imgSecond.classList.remove('_active-img');
+      // },
+      onEnterBack() {
+        imgSecond.classList.add('_active-img');
+      },
+      onLeaveBack() {
+        imgSecond.classList.remove('_active-img');
+      }
+    },
+    opacity: 1,
+  });
+
+  gsap.to('#img3', {
+    scrollTrigger: {
+      trigger: '#img3',
+      toggleActions: 'play reverse play reverse',
+      start: '+=2320s',
+      end: '+=3500s',
+      onEnter() {
+        imgThird.classList.add('_active-img');
+      },
+      onLeave() {
+        imgThird.classList.remove('_active-img');
+      },
+      onEnterBack() {
+        imgThird.classList.add('_active-img');
+      },
+      onLeaveBack() {
+        imgThird.classList.remove('_active-img');
+      }
+    },
+    opacity: 1,
+  });
+
+}
+
+setScrollText();
+setScrollImages();
+
+let mobWidth = window.matchMedia('(min-width: 1050px)');
+
+if (mobWidth.matches === true) {
+  gsap.to('.steps', {
+    scrollTrigger: {
+      pin: '.steps',
+      end: '+=4000s',
+      markers: false,
+      pinSpacing: true,
+    },
+  });
+}
+
+let headersHeight = document.querySelector('header').clientHeight + 'px'
+
+const setHeight = () => {
+  document.querySelector(".steps").style.minHeight = window.innerHeight + "px"
+};
+
+let deviceWidth = window.matchMedia("(max-width: 4000px)");
+if (deviceWidth.matches) {
+  window.addEventListener("resize", setHeight);
+  setHeight();
+}
+
+
+const stepsInner = document.querySelector('.steps__inner'),
+      stepsColumn = document.querySelector('.steps__column');
+
+let stepsInnerHeight = stepsInner.clientHeight;
+
+if (stepsInnerHeight > window.innerHeight) {
+  stepsColumn.style.gridGap = '35px';
+}
---
+++ source/js/common.js
@@ -0,0 +1,67 @@
+// const { init } = require("browser-sync");
+
+AOS.init({
+  once: true
+});
+const burger = document.querySelector('.burger')
+const mobMenu = document.querySelector('.m-menu')
+const body = document.getElementById('body'),
+      homeImage = document.querySelector('.home__image-wrapper');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('_active-burger')
+  if (burger.classList.contains('_active-burger')) {
+    mobMenu.classList.add('--active-menu')
+    body.classList.add('--body-locked')
+  } else {
+    mobMenu.classList.remove('--active-menu')
+    body.classList.remove('--body-locked')
+  }
+})
+
+const menuMobLinks = document.querySelectorAll('.menu-nav__link');
+
+if (menuMobLinks.length > 0) {
+  menuMobLinks.forEach(function (menuMobLinks) {
+    menuMobLinks.addEventListener("click", function (event) {
+      mobMenu.classList.remove('--active-menu');
+      body.classList.remove('--body-locked')
+      burger.classList.remove('_active-burger')
+    });
+  });
+}
+
+// window.onload = function(){
+//   const wrapper = document.getElementById('body'),
+//       layerTwo = document.querySelector('.home__image');
+
+//   wrapper.addEventListener('mousemove', (e) => {
+//     const pageX = e.clientX,
+//           pageY = e.clientY;
+
+//     layerTwo.style.transform = 'translateX(-' + pageX/250 + '%) translateY(-' + pageY/250 + '%)';
+//   });
+// }
+
+
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('.header');
+  header.classList.toggle('fixed', window.scrollY > 0);
+  let headerHeight = header.clientHeight;
+  const homeSection = document.querySelector('.home')
+  function homeSpacing() {
+    homeSection.style = `margin-top: ${headerHeight}px`;
+  }
+  if (header.classList.contains('fixed')) {
+    homeSpacing();
+  } else {
+    homeSection.style = 'margin-top: 0px';
+  }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
+  let headersHeight = header.clientHeight;
+    mobMenu.style.marginTop = `${headersHeight}px`
+})
+
